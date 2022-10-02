91ca8af32d687e62810cd13bcc080dfedd86f782
Gaurav Kumar Pandey <gaurav.golu1@gmail.com>
2022-10-02 09:41:45 +0530

    syntax improvements

--- env-to-json/env.js
+++ env-to-json/env.js
@@ -1,35 +1,34 @@
-const _fs = require("fs")
-const readline = require("readline")
+const _fs = require("fs");
+const readline = require("readline");
 
 function replaceAll(str, find, replace) {
 	var escapedFind = find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1")
 	return str.replace(new RegExp(escapedFind, "g"), replace)
-}
+};
+
+// Check if string is number
+function isNumeric(str) {
+	if (typeof str != "string") return false // we only process strings!
+	return (
+		!isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+		!isNaN(parseFloat(str))
+	) // ...and ensure strings of whitespace fail
+};
 
 async function processLineByLine() {
-	const fileStream = _fs.createReadStream(".env")
+	const fileStream = _fs.createReadStream(".env");
 
 	const rl = readline.createInterface({
 		input: fileStream,
 		crlfDelay: Infinity,
-	})
+	});
 
-	const object = {}
-
-
-	// Check if sting is number
-	function isNumeric(str) {
-		if (typeof str != "string") return false // we only process strings!
-		return (
-			!isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-			!isNaN(parseFloat(str))
-		) // ...and ensure strings of whitespace fail
-	}
+	const object = {};
 
 	for await (const line of rl) {
-		// Each line in input.txt will be successively available here as `line`.
+		// Each line in `input` will be successively available here as `line`.
 		if (line.startsWith("#") || line.length === 0) {
-			// console.log(`Skip: ${line}`)
+			console.log(`Skipped line : ${line}`);
 		} else {
 			const spiltVariable = line.split("=")
 			// console.log(myArray[0], myArray[1]);
@@ -46,8 +45,9 @@ async function processLineByLine() {
 			}
 		}
 	}
-	console.log(object)
 
-	_fs.writeFileSync("data.json", JSON.stringify(object))
+	console.log(object);
+
+	_fs.writeFileSync("env.json", JSON.stringify(object));
 }
 processLineByLine();
